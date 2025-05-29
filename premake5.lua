4616979f74b584d97af54d006f36ee48d388edfd
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-05-28 17:11:27 -0700

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,21 +1,8 @@
-project "ModernJSON"
+project "nlohmann_json"
     kind "StaticLib"
     language "C++"
     cppdialect "C++17"
     staticruntime "Off"
-
-    flags
-    {
-        "MultiProcessorCompile"
-    }
-
-    if OutputIntermediateDir == nil or OutputTargetDir == nil then
-        targetdir ("Build/bin/%{prj.name}/")
-        objdir    ("Build/obj/%{prj.name}/")
-    else
-        targetdir ("../../" .. OutputTargetDir .. "")
-        objdir    ("../../" .. OutputIntermediateDir .. "")
-    end
 
     files
     {
@@ -25,25 +12,8 @@ project "ModernJSON"
         "./**.md",
         "./**.lua",
     }
-
     includedirs
     {
         "./include",
         "./single_include"
     }
-
-    filter "configurations:Debug"
-        runtime "Debug"
-        buildoptions { "/MDd" } 
-        symbols "On"
-
-    filter "configurations:Optimized"
-        runtime "Release"
-        buildoptions { "/MDd" } 
-        optimize "On"
-
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "On"
-        buildoptions { "/MD" } 
-        symbols "Off"
